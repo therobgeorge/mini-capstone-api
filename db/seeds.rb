a8d5e410ab5f9694b84bced85ac7bc21db4b1897
therobgeorge <georg1rd@gmail.com>
2021-05-05 15:31:57 -0400

    creates data that needs to be seeded.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Taylor Swift - 1989", price: 29, description: "Taylor Swift - 1989 on black vinyl" image_url: "https://upload.wikimedia.org/wikipedia/en/f/f6/Taylor_Swift_-_1989.png")
+
+Product.create(name: "Jetty Bones - Push Back", price: 18, image_url: "https://target.scene7.com/is/image/Target/GUEST_e7d5e77a-14a9-4771-8098-dcb61b91c817?wid=488&hei=488&fmt=pjpeg")
+
