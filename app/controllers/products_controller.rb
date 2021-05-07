5328413bff97055d2ba89af1167dcd489500c9a6
therobgeorge <georg1rd@gmail.com>
2021-05-07 13:43:38 -0400

    adds find by product id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,14 @@ class ProductsController < ApplicationController
     product = Product.first
     render json: product.as_json
   end
+
+  def product_query
+    product = params[:product_id]
+    render json: Product.find(product).as_json
+  end
+
+  def product_name
+    product = params[:name]
+    render json: Product.find_by(:name "#{product}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
   get "/one_product", controller: "products", action: "one_product"
+  get "/product_query", controller: "products", action: "product_query"
+  get "/product_url/:product_id", controller: "products", action: "product_query"
+  get ""
 end
