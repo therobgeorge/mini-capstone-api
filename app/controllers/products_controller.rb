13dfc6620284e702db7724286550fb665ff5325c
therobgeorge <georg1rd@gmail.com>
2021-05-10 15:59:27 -0400

    adds create route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,17 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    product.save
+    render json: product.as_json
+  end
  
   def show
     product = params[:id]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/products", controller: "products", action: "index"
+  post "/products", controller: "products", action: "create"
   get "/products/:id", controller: "products", action: "show"
 end
