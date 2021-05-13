c83d192ed19d222b37fcf8af4d08f2a767ef3dd2
therobgeorge <georg1rd@gmail.com>
2021-05-13 17:09:56 -0400

    adds validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, :numericality {greater than 0}
+  validates :description, length: {in: 10..500}
+  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg|gif)$}i, :message => "must have a valid file type", :multiline => true
+  
   def is_discounted?
     if price < 30
       true
