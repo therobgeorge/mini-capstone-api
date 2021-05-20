2e066604ab85f6a1f73f82fcefb816a80bc11103
therobgeorge <georg1rd@gmail.com>
2021-05-20 16:41:20 -0400

    adds to application controller

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :image_url, :description, :is_discounted?, :tax, :total, :quantity, :supplier
+  attributes :id, :name, :price, :description, :is_discounted?, :tax, :total, :quantity, :supplier, :images
 end
