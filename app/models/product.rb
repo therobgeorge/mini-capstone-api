73e2ea8e111d0b9e1f7e376e623c57094be48769
therobgeorge <georg1rd@gmail.com>
2021-05-24 16:19:33 -0400

    adds has_many through associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders
   has_many :category_products
+  has_many :categories, through: :category_products
 
   # validates :name, presence: true, uniqueness: true
   # validates :price, :numericality {greater than 0}
